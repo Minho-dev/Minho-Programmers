0479c443e2817585a1727d91bbc52ff023d4c717
Minho13 <52019553+Minho13@users.noreply.github.com>
2019-09-27 09:59:02 +0900

    Add files via upload

---
+++ 같은_숫자는_싫어.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    answer = []
+    for x in range(len(arr)):
+        if x != len(arr) - 1:
+            if arr[x] != arr[x+1]:
+                answer.append(arr[x])
+    answer.append(arr[-1])
+    return answer
